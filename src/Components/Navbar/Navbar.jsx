7923036724f658ff01b5926a22f99025b267ca3a
unknown <Bawa Ernest Kwame>
2024-10-29 15:38:42 +0000

    scroll update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -22,14 +22,14 @@ const Navbar = () => {
       <ul>
         <li><Link to='hero' smooth={true} offset={0} duration={500} >Home</Link> </li>
 
-        <li><Link to='about' smooth={true} offset={0} duration={500}>About Us</Link> </li>
+        <li><Link to='about' smooth={true} offset={-260} duration={500}>About Us</Link> </li>
 
-        <li><Link to='program' smooth={true} offset={0} duration={500}>Programs</Link></li>
+        <li><Link to='program' smooth={true} offset={-150} duration={500}>Programs</Link></li>
 
-        <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link> </li>
+        <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link> </li>
 
-        <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link> </li>
-        <li><button className="btn">Contact Us</button></li>
+        <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link> </li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500}><button className="btn">Contact Us</button></Link> </li>
       </ul>
     </nav>
   );
